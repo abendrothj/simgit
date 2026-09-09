f1d4c51596cf451a1855477c345f6f14593e7204
Jake Abendroth <contact@jakea.net>
2026-09-08 18:41:53 -0700

    chore: point Homebrew formula at v0.1.8 binaries

--- packaging/homebrew/simgit.rb
+++ packaging/homebrew/simgit.rb
@@ -8,28 +8,28 @@
 class Simgit < Formula
   desc "Cheap, isolated copy-on-write Git worktrees for running many agents at once"
   homepage "https://github.com/abendrothj/simgit"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/abendrothj/simgit/releases/download/v0.1.7/sg-aarch64-apple-darwin.tar.gz"
-      sha256 "11ec1a9aa1ae3960d70f9ce30c4194e6d7e36eb5306d948a10bbeb0acc56ed1e"
+      url "https://github.com/abendrothj/simgit/releases/download/v0.1.8/sg-aarch64-apple-darwin.tar.gz"
+      sha256 "58b08f2440f14ab556daaae31c6d2b3a2844ebd5a5965a25add73900415ab76c"
     end
     on_intel do
-      url "https://github.com/abendrothj/simgit/releases/download/v0.1.7/sg-x86_64-apple-darwin.tar.gz"
-      sha256 "aa264706033464ff833d7322e90b471704cd1179a18bd19095332f8501f87b6f"
+      url "https://github.com/abendrothj/simgit/releases/download/v0.1.8/sg-x86_64-apple-darwin.tar.gz"
+      sha256 "26e4bb4935910f8223688c1599a7091727612e2f386756bb217600a5d65aa90e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/abendrothj/simgit/releases/download/v0.1.7/sg-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ca96ef55eeec23b40cfe4263fbbcef7bc183954bab961bdbc43ff68999f32d6a"
+      url "https://github.com/abendrothj/simgit/releases/download/v0.1.8/sg-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "d50e1eb7a05ae4c00875c4ce996e733a890d6f5fa0a544c12d84c0d6ab0d07ae"
     end
     on_intel do
-      url "https://github.com/abendrothj/simgit/releases/download/v0.1.7/sg-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "eb8f43101670074de0cd333e69527f71b859c6f7cf3c3113749fd4822e0db17c"
+      url "https://github.com/abendrothj/simgit/releases/download/v0.1.8/sg-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "6c476451ab87ab3c4d27b34236a874ff1e85d7b7bfea157e69c5c7da09b0da9a"
     end
   end
 
